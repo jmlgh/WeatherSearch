a276cfb02b121dd700f44dd246a7972f0ad2acbd
Javi Martinez <jfmarli@gmail.com>
2018-06-24 09:36:27 +0200

    temperature is now celsius and average line

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -6,16 +6,16 @@ class WeatherList extends Component {
 
     renderWeather(cityData) {
         const name = cityData.city.name
-        const temps = cityData.list.map(weather => weather.main.temp )
+        const temps = _.map(cityData.list.map(weather => weather.main.temp ), (temp) => temp - 273)
         const pressures = cityData.list.map(weather => weather.main.pressure )
         const humidities = cityData.list.map(weather => weather.main.humidity )
 
         return(
             <tr key={ name }>
                 <td>{ name } </td>
-                <td> <Chart data={ temps } color='red'/> </td>
-                <td> <Chart data={ pressures } color='green'/> </td>
-                <td> <Chart data={ humidities } color='black'/> </td>
+                <td> <Chart data={ temps } color='red' units="ºC"/> </td>
+                <td> <Chart data={ pressures } color='green' units="hPa"/> </td>
+                <td> <Chart data={ humidities } color='black' units="%" /> </td>
             </tr>
         )
     }
@@ -26,7 +26,7 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature (K)</th>
+                        <th>Temperature (ºC)</th>
                         <th>Pressure (hPa)</th>
                         <th>Humidity (%)</th>
                     </tr>
